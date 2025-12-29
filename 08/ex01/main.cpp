49b891ac5ea9dd79eb45beb1b71243b68c8698fb
sudaban <sumutdaban@gmail.com>
2025-12-29 18:19:32 +0300

    added more tests to 08/ex01/

--- 08/ex01/main.cpp
+++ 08/ex01/main.cpp
@@ -4,10 +4,56 @@
 
 int main()
 {
-	std::srand(std::time(NULL));	
-	Span sp = Span(100000);
-	sp.addRandomNumbersToArray(-5000, 5000, 100000);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-	return 0;
+    std::srand(std::time(NULL));
+
+
+    {
+        Span sp(5);
+        sp.addNumber(6);
+        sp.addNumber(3);
+        sp.addNumber(17);
+        sp.addNumber(9);
+        sp.addNumber(11);
+
+        std::cout << "[TEST 1] shortest = " << sp.shortestSpan() << std::endl;
+        std::cout << "[TEST 1] longest  = " << sp.longestSpan() << std::endl;
+    }
+
+    {
+        Span sp(1000);
+        sp.addRandomNumbersToArray(-100, 100, 1000);
+
+        std::cout << "[TEST 2] shortest = " << sp.shortestSpan() << std::endl;
+        std::cout << "[TEST 2] longest  = " << sp.longestSpan() << std::endl;
+    }
+
+    {
+        try
+        {
+            Span sp(2);
+            sp.addNumber(42);
+            sp.shortestSpan(); //should throw
+        }
+        catch (const std::exception &e)
+        {
+            std::cout << "[TEST 3] exception = " << e.what() << std::endl;
+        }
+    }
+
+    {
+        try
+        {
+            Span sp(3);
+            sp.addNumber(1);
+            sp.addNumber(2);
+            sp.addNumber(3);
+            sp.addRandomNumbersToArray(0, 10, 1); //should throw
+        }
+        catch (const std::exception &e)
+        {
+            std::cout << "[TEST 4] exception = " << e.what() << std::endl;
+        }
+    }
+
+    return 0;
 }
